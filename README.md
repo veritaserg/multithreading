a1cd8af6cb56cfd6009b8ccb3ac51c0d5c2130eb
veritaserg <infophotomasterscomua@gmail.com>
2018-10-03 10:29:19 +0300

    test

---
+++ README.md
@@ -0,0 +1,11 @@
+Objective
+
+
+This application in which files are input.
+
+You must determine the extension and size of each file.
+Each file is processed in a separate thread.
+
+At the same time, no more than 4 files can be processed.
+
+If the number of files is more - the rest of the files should be in the queue.
---
+++ src/main/Multithreading.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Multithreading {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\server\\IdeaProjects\\ Multithreading\\src\\resources");
+        List<File> files = new ArrayList<>();
+        files.add(file);
+
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        for (File f : files) {
+            executor.submit(
+                    () -> {
+                        String name = f.getName();
+                        System.out.println((name.substring(name.lastIndexOf(".") + 1) + " " + f.length()));
+                    }
+            );
+        }
+    }
+}
+
